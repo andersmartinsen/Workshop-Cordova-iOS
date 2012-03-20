ead151b20d348377f28ed3160f724a579f801bd7
Anders Martinsen <anders.martinsen@bekk.no>
2012-03-20 18:54:25 +0100

    [Oppgaver] Hentet oppgavetekst fra readme.md og lagt inn i js-fila og nummerert og kommentert mer.

--- workshop.cordova.ios/www/javascripts/app-oppgave.js
+++ workshop.cordova.ios/www/javascripts/app-oppgave.js
@@ -39,8 +39,10 @@
 	        self.searchByTwitterName(self.searchUsernameField.val());
       });
 		
-	  self.recordButton.on("click", function(e){
-        // Ta opp et lydklipp og spill det av
+	  self.recordButton.on("click", function(e) {
+		// Oppgave 5
+        // Her skal du lage en enkel lydopptaker som bruker telefonens standard lydopptaksfunksjon. Du bruker phonegap sitt Capture API for å ta opp lyden. 
+		// Lag et Media-objekt av opptaket og spill det av. Du velger selv om du vil lage en ny metode i App for dette, eller om du bare vil legge det direkte på riktig sted i setupBindings().
       });
     },
 	renderTweets: function(tweets){
@@ -65,10 +67,13 @@
       self.lagreAnsattTilKontaktLista(user);
     });
   },
-	lagreAnsattTilKontaktLista: function(user) {
-    	// Bruk kontakt-API'et til å lagre informasjonen i "user"-objektet til kontaktlista, f.eks bilde og navn  
-    },
-    searchByLocation: function(){
+	searchByLocation: function() {
+		/*	
+			Oppgave 2
+			Søke etter tweets i nærheten med geolokasjon. Her må du bruke PhoneGap sitt Geolocation API til å finne lokasjonen din i bredde- og lengdegrad som du legger på som parametre i den oppgitte twitter-url'en.
+			Bruk $.ajax() eller $.getJSON() som i oppgave 1 til å gjøre kallet. Du bruker renderTweets() og samme resultatside som i forrige oppgave.
+		*/
+		
 	  	// Legg på lokasjon i denne urlen
 	      var twitter_api_url = 'http://search.twitter.com/search.json?geocode=';
 	      // Hent tweets basert på lokasjon i json-format
@@ -80,24 +85,18 @@
 	      self.renderTweets(data.results);
 	},
 	searchByLocationWithNetworkCheck: function() {
+		/*
+			Oppgave 4
+			Dette er en utvidelse av oppgave 2 hvor man før man henter tweets basert på geolocation, sjekker om telefonen har nettverk. Dette gjøres via PhoneGap sitt Connection-API.
+			Hvis man ikke har nettverk, bruk PhoneGap sitt Notification-API til å gi en fornuftig feilmelding. Ellers vis tweets by geolokasjon.
+		*/
 		console.log("Searching for geolocation with network check")
 		var self = this;
 		checkConnection();
 	    
 		function checkConnection() {
-		  	var networkState = navigator.network.connection.type;
-		
-			if (networkState == Connection.UNKNOWN || networkState == Connection.NONE) {
-				console.log("No network. Returns an error alert");
-				navigator.notification.alert(
-				            'Could not fetch tweets since you are not connected to a network',
-				            alertDismissed,
-				            'Network problems',            
-				            'Done'
-						);
-			} else {
-				findGeoLocationWithPhoneGap();
-			}
+			// Bruk PhoneGap til å sjekke om du har tilgang til nettverk
+			// Hvis nei, hvis feilmelding, ellers vis tweets by geolocation med metoden findGeoLocationWithPhoneGap()
 		}
 		
 		function alertDismissed() {
@@ -105,26 +104,16 @@
 		}
 		
 		function findGeoLocationWithPhoneGap() {
-			console.log("Henter geolocation with PhoneGap")
-		    navigator.geolocation.getCurrentPosition(function(location)
-			{
-	        	var twitter_api_url = 'http://search.twitter.com/search.json?geocode=';
-	        	var latitude = location.coords.latitude;
-	        	var longitude = location.coords.longitude;
-	        	twitter_api_url += latitude + ',' + longitude + ',10km&rpp=5&show_user=true';
-
-	        	$.getJSON(twitter_api_url, function(data) {
-	          		if (data == undefined || data.results == undefined || data.results.length == 0){
-	            		navigator.notification.alert("No results for your location");
-	          		} else {
-	            		$.mobile.changePage(self.resultsPage);
-	            		self.renderTweets(data.results);
-	          		}
-	        	});
-	      		} , function(error) {console.log("Something went wrong when fetching tweets by geolocation")});
-	 		}
+			// Denne metoden blir lik som metoden searchByLocation() som du fullførte i oppgave 2
 		},
 		searchByTwitterName: function(username) {
+			/*
+			Oppgave 3, første del
+			Her skal man kunne søke etter et brukernavn på twitter og så legge til kontaktinfo (bilde og navn i det minste) i kontaktlista på telefonen.
+			I denne metoden lager du selve twitter-søket. Metoden renderUser() tar inn objektet du får fra twitter og skriver du ut i html'en.
+			I lagreAnsattTilKontaktLista() bruker du phonegap til å lagre brukeren i kontaktlista. Husk å bytt til "vis-bruker"-siden.
+			*/
+			
 			var searchUrl = 'http://api.twitter.com/1/users/show.json?callback=?&screen_name=' + username;
 
 		      // Søk etter bruker vha. $.ajax()
@@ -133,7 +122,18 @@
 		      $.mobile.changePage(self.usernameResultPage);
 		      self.renderUser(data); 
 		  	},
+			lagreAnsattTilKontaktLista: function(user) {
+				// Oppgave 3, andre del
+				// Denne delen består av implementere metoden searchByTwitterName() som ligg
+		    	// Bruk kontakt-API'et til PhoneGap å lagre informasjonen i "user"-objektet til kontaktlista, f.eks bilde og navn
+		    },
     search: function(keyword) {
+		/*
+		Oppgave 1
+		Søk etter keywords på twitter og vis dem i en liste med tweets. Søkefeltet og søkeknappen er allerede knyttet opp til riktige metoder i App-objektet. 
+		Du må altså hente ut søkeordet fra søkefeltet og bruke $.ajax() til å hente svar i JSON-format. Dette sender du så til renderTweets(tweets) og bytter til 
+		resultatsiden (som også er ett attributt på App).
+		*/
      	var searchUrl = 'http://search.twitter.com/search.json?callback=?&q=' + keyword;
 
 	      // Bruk $.ajax() til å søke etter tweets
